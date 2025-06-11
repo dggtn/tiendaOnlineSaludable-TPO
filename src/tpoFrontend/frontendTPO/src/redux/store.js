208da8707949971b7cf1bb889325b04cba77ad7c
Daniela <dggtn97@gmail.com>
2025-06-11 10:54:26 -0300

    ejemplo redux con productos

--- src/tpoFrontend/frontendTPO/src/redux/store.js
+++ src/tpoFrontend/frontendTPO/src/redux/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import productoReducer  from "./productosSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {productos:productoReducer},
 });
+
+//puedo administarr distintas partes del estado de forma separada, cada slice se ocupa de una area especifica de la logica de la app ,asi  su estado esta disponible de forma global
+
