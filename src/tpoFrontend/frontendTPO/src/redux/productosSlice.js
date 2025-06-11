504d8e79943f05a7b8211e3186ccabe42b03053e
Daniela <dggtn97@gmail.com>
2025-06-11 10:42:29 -0300

    agrego ejemplo de reducer productos

--- src/tpoFrontend/frontendTPO/src/redux/productosSlice.js
+++ src/tpoFrontend/frontendTPO/src/redux/productosSlice.js
@@ -7,3 +7,28 @@ export const fetchProductos = createAsyncThunk("productos/fetchProductos",async(
     return data
     
 } )
+
+const productoSlice = createSlice({
+    name: "productos",
+    initialState:{
+        items:[], //coincide con el use state del fetch que hicimos,
+        loading: false, //se puede agregar spinner o frase 'cargando'
+        error: null //los errores que ocurren se almacenan
+    },
+       reducers:{//funciones permiten actualizar estado en forma sincrona ,llamadas api son sincronas van vacias}
+        extraReducers: (builder)=>{
+            builder//define como va a reaccionar slice a reacciones asincronas como fetch/post define que va a pasar cuando conecte a la api
+                       //parametro es builder
+                       .addCase(fetchProductos.pending,(state)=>{
+                        state.loading=true;
+                        state.error=null
+
+                       })
+                       .addCase(fetchProductos.fulfilled,(state,action)=>{
+                        state.loading=fasle;
+                        sate.items = action.payload
+                       })
+
+        }
+
+})
