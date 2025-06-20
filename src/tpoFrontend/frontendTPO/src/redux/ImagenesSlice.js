fad57cdef17ac20d004bb9c2cf96b4ed7852e755
KeilaCheca <kchecaorihuela@uade.edu.ar>
2025-06-20 09:55:51 -0300

    agrego reducer para crear imagen

--- src/tpoFrontend/frontendTPO/src/redux/ImagenesSlice.js
+++ src/tpoFrontend/frontendTPO/src/redux/ImagenesSlice.js
@@ -9,6 +9,29 @@ export const fetchImagenesProducto = createAsyncThunk(
     }
 );
 
+export const createImagen = createAsyncThunk(
+    "imagenes/createImagen",
+    async ({ id, imagenFile, token }) => {
+        const formData = new FormData();
+        formData.append("file", imagenFile);
+        formData.append("productoId", id);
+
+        const { data } = await axios.post(
+            `http://localhost:4002/productos/${id}/imagen`,
+            formData,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "multipart/form-data",
+                },
+            }
+        );
+        return data; 
+    }
+);
+
+
+
 const imagenesSlice = createSlice({
     name: "imagenes",
     initialState: {
@@ -29,6 +52,19 @@ const imagenesSlice = createSlice({
         })
         .addCase(fetchImagenesProducto.rejected, (state, action) => {
             state.loading = false;
+            state.error = action.error.message;
+        })
+
+        .addCase(createImagen.pending, (state) => {
+            state.loading = true;   
+            state.error = null;    
+        })
+        .addCase(createImagen.fulfilled, (state, action) => {
+            state.items = [...state.items, action.payload];
+        })
+
+        .addCase(createImagen.rejected, (state, action) => {
+            state.loading = false;  
             state.error = action.error.message;
         });
     },
