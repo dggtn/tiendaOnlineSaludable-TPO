711e973844dfb62f0b8d7dd646ed62c893a0189f
Daniela <dggtn97@gmail.com>
2025-06-20 16:20:51 -0300

    creo carritoSlice

---
+++ src/tpoFrontend/frontendTPO/src/redux/carritoSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+
+export const carritoSlice = createSlice({
+    name:"carrito",
+    initialState:{},
+    reducers:{
+        
+    }
+})
+
+
+export default carritoSlice.reducer;
